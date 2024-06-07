2a09fe6ed7bc57c0b7ec244f5e27f6cc9bf9128a
Lluis Bonet <lluis.bonet@convidad.es>
2024-06-07 14:24:49 +0200

    use repository pattern and create endopint

--- NZWalks/NZWalks.API/Controllers/WalksController.cs
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
 {
@@ -11,20 +12,22 @@ namespace NZWalks.API.Controllers
     public class WalksController : ControllerBase
     {
         private readonly IMapper mapper;
+        private readonly IWalkRepository walkRepository;
 
-        public WalksController(IMapper mapper)
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
         {
             this.mapper = mapper;
+            this.walkRepository = walkRepository;
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] AddWalkRequestDto addWalkRequestDto)
+        public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
         {
             var walkDomain = mapper.Map<Walk>(addWalkRequestDto);
+            var createdWalk = await walkRepository.CreateAsync(walkDomain);
+            var walkDto = mapper.Map<WalkDto>(createdWalk);
 
-
-
-            return Ok();
+            return Ok(walkDto);
         }
     }
 }
