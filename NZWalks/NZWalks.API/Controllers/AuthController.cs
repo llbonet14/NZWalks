10018c6804a213c7b7c12c04b13ad80dc9576d3e
Lluis Bonet <lluis.bonet@convidad.es>
2024-06-13 12:46:03 +0200

    register users

---
+++ NZWalks/NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (!identityResult.Succeeded)
+                return BadRequest("Something went wrong");
+
+            if (registerRequestDto.Roles is not null && registerRequestDto.Roles.Length != 0)
+                identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+            if (!identityResult.Succeeded)
+                return BadRequest("Something went wrong");
+
+            return Ok("User was registered");
+        }
+
+        //[HttpPost]
+        //[Route("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        //{
+        //    return Ok();
+        //}
+    }
+}
