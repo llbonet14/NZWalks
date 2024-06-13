6b32cc9a157a49db9449b40a370bcb2fbb415fa8
Lluis Bonet <lluis.bonet@convidad.es>
2024-06-13 12:58:01 +0200

    generate token

--- NZWalks/NZWalks.API/Repositories/TokenRepository.cs
+++ NZWalks/NZWalks.API/Repositories/TokenRepository.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NZWalks.API.Repositories
 {
     public class TokenRepository : ITokenRepository
     {
+        private readonly IConfiguration configuration;
+
+        public TokenRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public string CreateJWTToken(IdentityUser user, List<string> roles)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Email, user.Email)
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:Issuer"],
+                audience: configuration["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
