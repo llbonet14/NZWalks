7772b9e98007e13348f939355631d3b52a045b0b
Lluis Bonet <lluis.bonet@convidad.es>
2024-06-13 12:52:24 +0200

    create token service

---
+++ NZWalks/NZWalks.API/Repositories/Class.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.API.Repositories
+{
+    public class Class : ITokenRepository
+    {
+        public string CreateJWTToken(IdentityUser user, List<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
