186647d8be98c9a44148ce9862cf5a27a67b2191
Lluis Bonet <lluis.bonet@convidad.es>
2024-06-13 12:03:20 +0200

    create db and seed data

--- NZWalks/NZWalks.API/Data/NZIdentityDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZIdentityDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -7,6 +8,22 @@ namespace NZWalks.API.Data
     {
         public NZIdentityDbContext(DbContextOptions<NZIdentityDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var readerRoleId = "809838d4-4252-4bf6-9fc6-231fa6142fe3";
+            var writerRoleId = "2c2bd542-7674-47f2-a428-68358f1d5f3";
+
+            var roles = new List<IdentityRole>
+            {
+                new() {Id = readerRoleId, ConcurrencyStamp = readerRoleId, Name = "Reader", NormalizedName = "READER"},
+                new() {Id = writerRoleId, ConcurrencyStamp = writerRoleId, Name = "Writer", NormalizedName = "WRITER"}
+            };
+
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
